2a69e7a6d6f38857bdb00042c0b6c3ea2c50f6c3
dingliucheng <fengsheng_0813@qq.com>
2026-06-25 00:13:37 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '3ed6053553a04e0698a981797052d7cb',
+  rootNotionPageId: '3892d716a0ff809283fef157743d6f80',
 
   // It's will be used as the root page when running `yarn dev`. (optional)
   // If you are suffering from the slow loading or compiling speed, set this to a smaller page.
